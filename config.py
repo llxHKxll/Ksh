88dea2b4e9fb96aca401f6b64dc1afb43ed223af
🦋⃟ᬊ 𝗛ᴋ <161941135+llxHKxll@users.noreply.github.com>
2024-12-09 19:33:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from pyrogram import Client
 
 API_ID = "21989020"
 API_HASH = "3959305ae244126404702aa5068ba15c"
-BOT_TOKEN = "8141351816:AAG1_YB0l88X0SLAHnos9iODdZuCdNEfuFo"
+BOT_TOKEN = "7612524821:AAEpYgNa48gQpavBut4UiWow7L774RVsrvY"
 
 app = Client(
   name="Kaisen Ranking Bot",
