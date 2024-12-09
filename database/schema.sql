0f396b0da0d71b56a5f91777b64a27b5b4b3af8a
🦋⃟ᬊ 𝗛ᴋ <161941135+llxHKxll@users.noreply.github.com>
2024-12-09 18:07:13 +0530

    Create schema.sql

---
+++ database/schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS users (
+                user_id INTEGER PRIMARY KEY,
+                username TEXT,
+                points INTEGER DEFAULT 10000,
+                level INTEGER DEFAULT 1,
+                exp INTEGER DEFAULT 0,
+                health INTEGER DEFAULT 100,
+                last_activity_time INTEGER DEFAULT 0,
+                last_claimed INTEGER DEFAULT 0,
+                chat_id INTEGER DEFAULT 0,
+                xp_booster_expiry INTEGER DEFAULT 0  -- Add this column to track booster expiry
+            );
