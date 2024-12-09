f1d3a727f949d259af39a94e0b7804a2fe77de41
🦋⃟ᬊ 𝗛ᴋ <161941135+llxHKxll@users.noreply.github.com>
2024-12-09 16:39:35 +0530

    Update start.py

--- Bot/modules/start.py
+++ Bot/modules/start.py
@@ -1,3 +1,4 @@
+from config import app
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from database.db_manager import ensure_user_exists  # To check if the user exists in the database
